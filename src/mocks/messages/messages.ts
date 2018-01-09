feb62fa31619398097ff4dff6900523a95c17fb8
Alí Ghanbari <ali.ghanbari@babel.es>
2018-01-09 17:00:14 +0100

    Files added

---
+++ src/mocks/messages/messages.ts
@@ -0,0 +1,6 @@
+import {Message } from '../../models/messages/message';
+import { User } from '../../models/user/user';
+
+const userList: User[]=[
+        {firstName:'Pepe',lastName:'Garcia',email:'pepe.garcia@gmail.com',avatar:'assets/imgs/avatar.png' }
+]
